c824d813acc54ddb40439ea93c5d049e62ae3126
BotalovSN <ctulhufromrlieh@gmail.com>
2023-12-09 00:08:26 +0500

    - Added initial collapse on low height windows only if javascript enabled

--- js/main.js
+++ js/main.js
@@ -1,14 +1,25 @@
+function toggleCollapsible(elem){
+    if (window.getComputedStyle(elem, null).display === "none") {
+        elem.style.display = "flex";
+    } else {
+        elem.style.display = "none";
+    }    
+}
+
 let collapsators = document.getElementsByClassName("collapsator");
 
 for (let currCollapsatorIndex = 0; currCollapsatorIndex < collapsators.length; currCollapsatorIndex++) {
     collapsators[currCollapsatorIndex].addEventListener("click", function() {
-        // this.classList.toggle("active");
-        let content = this.nextElementSibling;
-        // if (content.style.display === "none") {
-        if (window.getComputedStyle(content, null).display === "none") {
-            content.style.display = "flex";
-        } else {
-            content.style.display = "none";
-        }
+        let collapsible = this.nextElementSibling;
+        toggleCollapsible(collapsible);
     });
+}
+
+let collapsibles = document.getElementsByClassName("collapsible");
+
+for (let currCollapsibleIndex = 0; currCollapsibleIndex < collapsators.length; currCollapsibleIndex++) {
+    let collapsible = collapsibles[currCollapsibleIndex]
+    if (getComputedStyle(collapsible).getPropertyValue('--collapse-on-start') == 'true'){
+        toggleCollapsible(collapsible)
+    }
 }
